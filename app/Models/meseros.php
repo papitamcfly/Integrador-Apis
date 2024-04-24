0c96e7f4212e62bae98abb0f503bd41e005ae831
GustavoContreras511 <gustavoangelcontreraslopez@gmail.com>
2024-04-24 17:45:42 -0600

    mesero push

--- app/Models/meseros.php
+++ app/Models/meseros.php
@@ -5,9 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model;
 
-class meseros extends Model
+class  meseros extends Model
 {
     use HasFactory;
+
     protected $connection = 'mongodb';
-    protected $collection = 'mesero';
+
+    protected $collection = 'meseros';
+
+    protected $fillable = [
+        'id',
+        'userId',
+        'nombre',
+    ];
+
+    public $incrementing = false;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
 }
